097c48e181b607fde8fbaa2bc05fda5aaeb74b9c
Web8Trace <chdennik@yandex.ru>
2022-11-09 18:26:26 +0300

    tests done3

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -13,6 +13,7 @@ import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,34 +51,30 @@ class UserControllerTest {
         @Test
     void postAndGetUsers() throws ValidationException, NotFoundException {
         UserController userController=new UserController(new UserService(new InMemoryUserStorage()));
-        List<User> userList=new ArrayList<>();
         User user=new User();
         user.setName("user");
         user.setBirthday(LocalDate.parse("1975-01-01"));
         user.setEmail("user@email.com");
         user.setLogin("corUser");
-        userList.add(user);
         userController.postUser(user);
-        Assertions.assertEquals(userList, userController.getUsers());
+        Assertions.assertEquals(user, userController.getUser(user.getId()));
 
     }
     @Test
     void putUser() throws ValidationException, NotFoundException {
         UserController userController=new UserController(new UserService(new InMemoryUserStorage()));
-        List<User> userList=new ArrayList<>();
         User user=new User();
         user.setName("user");
         user.setBirthday(LocalDate.parse("1975-01-01"));
         user.setEmail("user@email.com");
         user.setLogin("corUser");
         userController.postUser(user);
-        user=new User();
-        user.setBirthday(LocalDate.parse("1985-01-01"));
-        user.setEmail("usere@email.com");
-        user.setLogin("corUser");
-        user.setId(1L);
-        userList.add(user);
-        userController.putUser(user);
-        Assertions.assertEquals(userList, userController.getUsers());
+        User user1=new User();
+        user1.setBirthday(LocalDate.parse("1985-01-01"));
+        user1.setEmail("usere@email.com");
+        user1.setLogin("corUser");
+        user1.setId(1L);
+        userController.putUser(user1);
+        Assertions.assertEquals(user1, userController.getUser(user1.getId()));
     }
 }
